a78d10c74e94ea63c14a3940e7bca24366e6d8c8
OrangeSodaMonster <fornagiero.vinicius@gmail.com>
2024-11-11 04:28:15 -0300

    Started Survival Events

---
+++ Assets/SurvivalAssets/EventsHolder.cs
@@ -0,0 +1,79 @@
+using MoreMountains.Feedbacks;
+using Sirenix.OdinInspector;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class EventsClass
+{
+    public List<MMF_Player> MyEvents = new List<MMF_Player>();
+}
+
+public class EventsHolder : MonoBehaviour
+{
+    [SerializeField] Transform EventsObj;
+    [SerializeField, GUIColor("cyan")] Transform PositiveEventsObj;
+    [SerializeField, GUIColor("red")] Transform EndEventsObj;
+    [Space]
+    public List<EventsClass> ListOfEvents = new();
+    public List<EventsClass> ListOfPositiveEvents = new();
+    public List<EventsClass> ListOfEndEvents = new();
+
+    [Button, PropertyOrder(-1), HorizontalGroup("1")]
+    void SetEvents()
+    {
+        ListOfEvents.Clear();
+        foreach(Transform child in EventsObj)
+        {
+            ListOfEvents.Add(new());
+            foreach (Transform childChild in child)
+            {
+                ListOfEvents[ListOfEvents.Count-1].MyEvents.Add(childChild.GetComponent<MMF_Player>());
+            }
+        }
+    }
+    [Button, GUIColor("cyan"), PropertyOrder(-1), HorizontalGroup("1")]
+    void SetPositiveEvents()
+    {
+        ListOfPositiveEvents.Clear();
+        foreach (Transform child in PositiveEventsObj)
+        {
+            ListOfPositiveEvents.Add(new());
+            foreach (Transform childChild in child)
+            {
+                ListOfPositiveEvents[ListOfPositiveEvents.Count-1].MyEvents.Add(childChild.GetComponent<MMF_Player>());
+            }
+        }
+    }
+    [Button, GUIColor("red"), PropertyOrder(-1), HorizontalGroup("1")]
+    void SetEndEvents()
+    {
+        ListOfEndEvents.Clear();
+        foreach (Transform child in EndEventsObj)
+        {
+            ListOfEndEvents.Add(new());
+            foreach (Transform childChild in child)
+            {
+                ListOfEndEvents[ListOfEndEvents.Count-1].MyEvents.Add(childChild.GetComponent<MMF_Player>());
+            }
+        }
+    }
+
+    [Button, HorizontalGroup("2")]
+    public void CallEvent(int level)
+    {
+        ListOfEvents[level].MyEvents[UnityEngine.Random.Range(0, ListOfEvents[level].MyEvents.Count-1)].PlayFeedbacks();
+    }
+    [Button, GUIColor("cyan"), HorizontalGroup("2")]
+    public void CallPositiveEvent(int level)
+    {
+        ListOfPositiveEvents[level].MyEvents[UnityEngine.Random.Range(0, ListOfEvents[level].MyEvents.Count-1)].PlayFeedbacks();
+    }
+    [Button, GUIColor("red"), HorizontalGroup("2")]
+    public void CallEndEvent(int level)
+    {
+        ListOfEndEvents[level].MyEvents[UnityEngine.Random.Range(0, ListOfEvents[level].MyEvents.Count-1)].PlayFeedbacks();
+    }
+}
--- Assets/SurvivalAssets/SurvivalTimer.cs
+++ Assets/SurvivalAssets/SurvivalTimer.cs
@@ -11,11 +11,15 @@ public class SurvivalSection
     [Serializable]
     public class SectionEvent
     {
+        [Tooltip("The Time For This Event To Happen")]
+        [HorizontalGroup("1")]
         public float Time;
+        [Tooltip("Event Difficult Level, Starts at 0")]
+        [MinValue(0)][HorizontalGroup("1")]
         public int Level;
     }
 
-    [HorizontalGroup("0"), MinValue(60)]
+    [HorizontalGroup("0"), MinValue(3)]
     public float Duration = 120;
     [HorizontalGroup("0"), MinValue(0)]
     public int NumPositiveEvents = 1;
@@ -26,30 +30,49 @@ public class SurvivalSection
     [HorizontalGroup("1"), MinValue(0)]
     public float EventTimeVariation = 10;
     public List<SectionEvent> Events;
-    [GUIColor("red")]
-    public SectionEvent EventAtEnd;
     [GUIColor("cyan")]
     [ShowIf("@NumPositiveEvents > 0")]
     public List<SectionEvent> PositiveEvents;
+    [GUIColor("red")]
+    public SectionEvent EndEvent;
     //Tempo para lidar com o último evento antes de iniciar a próxima seção
     [MinValue(0)]
     public float LastEventExtraTime = 20;
 }
 
+[RequireComponent(typeof(EventsHolder))]
 public class SurvivalTimer : MonoBehaviour
 {
     public List<SurvivalSection> Sections;
-
     public float TotalTime;
+    public int CurrentSection;
+
+    EventsHolder EventsHolder;
+
+    private void Awake()
+    {
+        EventsHolder = GetComponent<EventsHolder>();
+    }
+
+    private void Start()
+    {
+        SetTimers(useRandom:true);
+
+        #if UNITY_EDITOR
+            CheckEventLevels();
+        #endif
+    }
 
     private void Update()
     {
         TotalTime += Time.deltaTime;
+        CheckTimers();
     }
 
     private void OnValidate()
     {
         SetTimers();
+        CheckEventLevels();
     }
 
     [Button("SetTimers"), PropertyOrder(-1)]
@@ -77,7 +100,8 @@ public class SurvivalTimer : MonoBehaviour
                     Sections[i].Events[j].Time += UnityEngine.Random.Range(-Sections[i].EventTimeVariation, Sections[i].EventTimeVariation);
             }
 
-            //Positive Events
+            //Positive Events 
+            //TO DO: SET TIME BETWEEN EVENTS
             while (Sections[i].PositiveEvents.Count < Sections[i].NumPositiveEvents)
             {
                 Sections[i].PositiveEvents.Add(new SectionEvent());
@@ -95,9 +119,80 @@ public class SurvivalTimer : MonoBehaviour
                     Sections[i].PositiveEvents[j].Time += UnityEngine.Random.Range(-timeBetweenPositiveEvents, timeBetweenPositiveEvents);
             }
 
-            Sections[i].EventAtEnd.Time = Sections[i].Duration + totalSectionsDuration;
+            Sections[i].EndEvent.Time = Sections[i].Duration + totalSectionsDuration;
             totalSectionsDuration += Sections[i].Duration + Sections[i].LastEventExtraTime;
         }
     }
 
+    void CheckTimers()
+    {
+        for (int i = 0; i < Sections.Count; i++)
+        {   
+            //Events
+            for (int j = 0; j < Sections[i].Events.Count; j++)
+            {
+                if(TotalTime > Sections[i].Events[j].Time)
+                {
+                    EventsHolder.CallEvent(Sections[i].Events[j].Level);
+                    Sections[i].Events.RemoveAt(j);
+                }           
+            }
+            //Positive Events
+            for (int j = 0; j < Sections[i].PositiveEvents.Count; j++)
+            {
+                if (TotalTime > Sections[i].PositiveEvents[j].Time)
+                {
+                    EventsHolder.CallEvent(Sections[i].PositiveEvents[j].Level);
+                    Sections[i].PositiveEvents.RemoveAt(j);
+                }
+            }
+            //End Event
+            if (TotalTime > Sections[i].EndEvent.Time)
+            {
+                EventsHolder.CallEvent(Sections[i].EndEvent.Level);
+                Sections[i].EndEvent.Time = float.MaxValue;
+            }
+            //Change Section
+            if (TotalTime > Sections[i].EndEvent.Time + Sections[i].LastEventExtraTime)
+            {
+                CurrentSection++;
+                Sections.RemoveAt(i);
+            }
+        }       
+    }
+
+    void CheckEventLevels()
+    {
+        EventsHolder = GetComponent<EventsHolder>();
+
+        for (int i = 0; i < Sections.Count; i++)
+        {
+            for (int j = 0; j < Sections[i].Events.Count; j++)
+            {
+                if (Sections[i].Events[j].Level > EventsHolder.ListOfEvents.Count-1)
+                {
+                    Debug.Log($"<color=orange> Event Level Out Of Range </color>");
+                }
+            }
+        }
+
+        for (int i = 0; i < Sections.Count; i++)
+        {
+            for (int j = 0; j < Sections[i].PositiveEvents.Count; j++)
+            {
+                if (Sections[i].PositiveEvents[j].Level > EventsHolder.ListOfPositiveEvents.Count-1)
+                {
+                    Debug.Log($"<color=orange> Positive Event Level Out Of Range </color>");
+                }
+            }
+        }
+
+        for (int i = 0; i < Sections.Count; i++)
+        {
+            if (Sections[i].EndEvent.Level > EventsHolder.ListOfEndEvents.Count-1)
+            {
+                Debug.Log($"<color=orange> End Event Level Out Of Range </color>");
+            }
+        }
+    }
 }
